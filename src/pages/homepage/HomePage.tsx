6d8746b0086ff2aca4822b557c1861569258693d
Michael Gomeh <gomehmichael@gmail.com>
2025-02-26 12:40:53 +0200

    imported font

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,5 +1,14 @@
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<div>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque rerum
+			voluptate, optio molestias architecto nemo exercitationem nesciunt
+			deserunt error iste sint nulla totam tenetur atque reiciendis voluptatem
+			blanditiis perspiciatis, alias, molestiae reprehenderit harum! Fugiat rem
+			iusto fuga dignissimos nesciunt, porro maxime neque possimus in eos soluta
+			cum tempora illum libero!
+		</div>
+	);
 };
 
 export default HomePage;
