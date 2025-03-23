d7968929aaf96fdb9099051cfe1edaae4282be9c
Michael Gomeh <gomehmichael@gmail.com>
2025-03-23 12:56:06 +0200

    articles page category filtering

--- src/pages/articlespage/ArticlesPage.tsx
+++ src/pages/articlespage/ArticlesPage.tsx
@@ -1,19 +1,25 @@
-import {articles} from "../../data.ts";
-import ArticleLink from "../../components/article/ArticleLink.tsx";
-import styles from './ArticlesPage.module.css'
-import {useSearchParams} from "react-router";
+import { articles } from '../../data.ts';
+import ArticleLink from '../../components/article/ArticleLink.tsx';
+import styles from './ArticlesPage.module.css';
+import { useSearchParams } from 'react-router';
 
 const ArticlesPage = () => {
-    const [searchParams,] = useSearchParams();
-    const cat = searchParams.get("cat");
-    return (
-        <div className={styles.articlesList}>
-            <h2>{cat}</h2>
-            {articles.map((article) => (
-                <ArticleLink key={article.id} article={article} view="horizontal"/>
-            ))}
-        </div>
-    );
+	const [searchParams] = useSearchParams();
+	const cat = searchParams.get('cat');
+	return (
+		<div className={styles.articlesList}>
+			<h2 className="articles-category">{cat}</h2>
+			{articles
+				.filter(
+					(article) =>
+						article.category.toLowerCase() ===
+						cat?.replace('-', ' ').toLowerCase()
+				)
+				.map((article) => (
+					<ArticleLink key={article.id} article={article} view="horizontal" />
+				))}
+		</div>
+	);
 };
 
 export default ArticlesPage;
